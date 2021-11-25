86d8839d2e230e6bfae51c5b1a3ead046546c11e
Sue Lee <sue.lee0308@gmail.com>
2021-11-25 16:27:20 -0600

    random header works

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,51 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Header.css";
-import boba from '../styles/images/boba.png'
-import golf from '../styles/images/golf.png'
-import coke from '../styles/images/coke.png'
-import prague from '../styles/images/pragueclass.jpeg'
-import climbing from '../styles/images/climbing.jpeg'
-import greece from '../styles/images/greece.jpeg'
+
 
 export default function Header () {
 
+    const [index, setIndex] = useState(0);
+
     const randomFacts = [
         {
-            image: {boba},
+            image: './images/boba.png',
             fact: "My go-to boba order: milk tea, half sugar, less ice, with boba."
         },
         {
-            image: {golf},
+            image: './images/golf.png',
             fact: "The most famous person I've seen in person is Tiger Woods."
         },
         {
-            image: {coke},
+            image: './images/coke.png',
             fact: "I don't drink Diet Coke, Coke Zero nor any other variation of Coke. I only drink the real thing."
         },
         {
-            image: {prague},
+            image: './images/pragueclass.jpeg',
             fact: 'I studied abroad in Prague, Czech Republic for a semester. (Photo taken from my classroom)'
         },
         {
-            image: {climbing},
+            image: './images/climbing.jpeg',
             fact: 'I like to climb complicated rocks.'
         },
         {
-            image: {greece},
+            image: './images/greece.jpeg',
             fact: "My favorite place I've traveled to is Athens, Greece (the art history nerd in me)."
         },
     ]
 
+    function changeFact() {
+        if (index === randomFacts.length - 1) {
+            setIndex(0)
+        } else {
+        let newIndex = index + 1
+        setIndex(newIndex);
+        }
+    };
+
     return (
         <div className="Header">
-            {randomFacts.map((randomFact) =>
-               <div className="randomFacts">
-               <img src={randomFact.image} alt='random'/>
-               <h1>
-                   {randomFact.fact}
-               </h1>
-           </div>
-            )}
+            <div className="randomFacts" key={randomFacts[index].image}>
+                <button className='factButton' onClick={() => changeFact()}>
+                    <img className='buttonImg' src={randomFacts[index].image} alt='random'/>
+                </button>
+                <h1 className='factText'>
+                    {randomFacts[index].fact}
+                </h1>
+            </div>
         </div>
     )
 }
