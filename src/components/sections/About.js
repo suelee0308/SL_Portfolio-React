dbac363203a586ced00f8b114a3179f46851aa95
Sue Lee <sue.lee0308@gmail.com>
2022-01-25 16:55:15 -0600

    text

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -18,7 +18,7 @@ export default function About({loading}) {
             </div>
             <section className="aboutSection">
                 <p>
-                    I am a full stack web developer with a good eye. I've always had a love for design and user experience. In my 8 years of teaching, this applied to how I operated my classroom. In web development, I've found that this not only applies to UX but also the code itself and how its structured and written.
+                    I am a full stack web developer with a good eye. I've always had a love for design and user experience. In my 8 years of teaching, this applied to how I operated my classroom. In web development, I've found that this not only applies to UX/UI but also the code itself and how its structured and written.
                 </p>
                 <p>
                     After graduating with a degree in education from the University of Texas at Austin and teaching little people, I bring leadership and effective communication skills to this world of web development. I am well versed in teamwork and being detail-oriented. I strive daily to make life easier for people - whether that be clients, my students, my family, or my dog.
